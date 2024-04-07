0abf4b10cbdc5299ff95a456f2053b92f9fcfbcb
BurovEgor <egorburov031@gmail.com>
2024-04-07 12:37:19 +0300

    1

---
+++ task1.py
@@ -0,0 +1,13 @@
+first_list = [True, '2', 3.3, 5, 4]
+
+del first_list[2]
+
+print(len(first_list))
+
+first_list.reverse()
+
+second_list = [6, 8]
+
+summ = first_list + second_list
+
+print(summ)
---
+++ task2.py
@@ -0,0 +1,9 @@
+
+first_list = [2, 3]
+second_list = [4, 5]
+
+summ1 = first_list + second_list
+summ2 = first_list.__add__(second_list)
+
+print(summ1)
+print(summ2)
